736fb1bf604a87cb49b05cc96a8c42f2f0fc9c5a
mulleragustin <agustinmuller98@gmail.com>
2024-09-02 20:00:08 -0300

    chore: Update home view to display member statistics
    ```

--- musinjer/views.py
+++ musinjer/views.py
@@ -1,6 +1,15 @@
 from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
+from socios.models import Socio
 
 @login_required
 def home(request):
-    return render(request, 'base.html')
+    active_members = Socio.objects.filter(estado='A').count()
+    suspended_members = Socio.objects.filter(estado='S').count()
+    inactive_members = Socio.objects.filter(estado='B').count()
+    ctx = {
+        'active_members': active_members, 
+        'suspended_members': suspended_members,
+        'inactive_members': inactive_members,
+    }
+    return render(request, 'base.html', ctx)
